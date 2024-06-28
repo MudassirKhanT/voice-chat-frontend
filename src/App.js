a9908c27c87de2e3ffd2f68dff90946bc520e91f
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-06-28 17:26:37 +0530

    Home Route Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to coders house</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return <div>This is our Home page</div>;
+};
+
+export default Home;
+
+/**
+ * return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </BrowserRouter>
+  );
+ */
