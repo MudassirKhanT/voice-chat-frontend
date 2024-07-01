5e952d8be8f2335551309f8464017c3343e388d9
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-01 17:12:19 +0530

    add email page

--- src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Card from "../../../../Components/shared/Card/Card";
+import Button from "../../../../Components/shared/Button/Button";
+import TextInput from "../../../../Components/shared/TextInput/TextInput";
+import styles from "../StepPhoneEmail.module.css";
+import { useState } from "react";
 const Email = () => {
-  return <div>This is email component</div>;
+  const [email, setemail] = useState("");
+  return (
+    <Card title="Enter your email id" icon="email-emoji">
+      <TextInput value={email} onChange={(e) => setemail(e.target.value)} />
+      <div>
+        <div className={styles.actionButtonWrap}>
+          <Button text="Next"></Button>
+        </div>
+        <p className={styles.bottomParagraph}>By entering your number, you’re agreeing to our Terms of Service and Privacy Policy. Thanks!</p>
+      </div>
+    </Card>
+  );
 };
 
 export default Email;
