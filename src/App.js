d7152ef508d0a10945e8e100572ddd63e0076324
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-01 15:48:54 +0530

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,110 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navigation from "./Components/shared/Navigation/Navigation";
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
+//import Register from "./pages/Register/Register";
+//import Login from "./pages/Login/Login";
+import Authenticate from "./pages/Authenticate/Authenticate";
+import Activate from "./pages/Activate/Activate";
+import Rooms from "./pages/Rooms/Rooms";
+
+const isAuth = true;
+const user = {
+  activated: true,
+};
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route
+          path="/"
+          element={
+            <GuestRoute>
+              <Home />
+            </GuestRoute>
+          }
+        />
+        <Route
+          path="/authenticate"
+          element={
+            <GuestRoute>
+              <Authenticate />
+            </GuestRoute>
+          }
+        /> */}
+        {/* 
+        <Route element={<GuestRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/authenticate" element={<Authenticate />} />
+        </Route> */}
+        <Route
+          path="/"
+          element={
+            <GuestRoute>
+              <Home />
+            </GuestRoute>
+          }
+        />
+
+        <Route
+          path="/authenticate"
+          element={
+            <GuestRoute>
+              <Authenticate />
+            </GuestRoute>
+          }
+        />
+
+        <Route
+          path="/activate"
+          element={
+            <SemiProtectedRoute>
+              <Activate />
+            </SemiProtectedRoute>
+          }
+        />
+        <Route
+          path="/rooms"
+          element={
+            <ProtectedRoute>
+              <Rooms />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
+//Reciving props as arguments
+const GuestRoute = ({ children }) => {
+  if (isAuth) {
+    return <Navigate to="/rooms" />;
+  } else {
+    return children;
+  }
+};
+const SemiProtectedRoute = ({ children }) => {
+  //protected routes
+  if (!isAuth) {
+    return <Navigate to="/" />;
+  } else if (isAuth && !user.activated) {
+    return children;
+  } else {
+    return <Navigate to="/rooms" />;
+  }
+};
+
+const ProtectedRoute = ({ children }) => {
+  //protected routes
+  if (!isAuth) {
+    return <Navigate to="/" />;
+  } else if (isAuth && !user.activated) {
+    return <Navigate to="/activate" />;
+  } else {
+    return children;
+  }
+};
 
 export default App;
