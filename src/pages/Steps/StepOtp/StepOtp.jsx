2632c6f40c65fc098682f6c83020b989849cccf1
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-04 20:35:49 +0530

    add otp step

--- src/pages/Steps/StepOtp/StepOtp.jsx
+++ src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../../../Components/shared/Card/Card";
+import styles from "./StepOtp.module.css";
+import TextInput from "../../../Components/shared/TextInput/TextInput";
+import Button from "../../../Components/shared/Button/Button";
 
 const StepOtp = ({ onNext }) => {
+  const [otp, StepOtp] = useState("");
+
+  async function submit() {}
   return (
     <>
-      <div>Otp component</div>
-      <button onClick={onNext}>Next</button>
+      <div className={styles.cardWrapper}>
+        <Card title="Enter the code we just texted you" icon="lock-emoji">
+          <TextInput value={otp} onChange={(e) => StepOtp(e.target.value)} />
+          <div className={styles.actionButtonWrap}>
+            <Button onClick={submit} text="Next" />
+          </div>
+          <p className={styles.bottomParagraph}>By entering the number, you're agreeing to our Terms of Service and Privacy Policy. Thanks!</p>
+        </Card>
+      </div>
     </>
   );
 };
