abfc8c1edf0fbf5c15efbd0ad73bd01f03cb2d05
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-09 20:31:33 +0530

    add Full name page

--- src/pages/Steps/StepName/StepName.jsx
+++ src/pages/Steps/StepName/StepName.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Card from "../../../Components/shared/Card/Card";
+import TextInput from "../../../Components/shared/TextInput/TextInput";
+import Button from "../../../Components/shared/Button/Button";
+import styles from "./StepName.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setName } from "../../../store/activateSlice";
 const StepName = ({ onNext }) => {
+  const { name } = useSelector((state) => state.activate);
+  const dispatch = useDispatch();
+  const [fullName, setfullName] = useState(name);
+  function nextStep() {
+    if (!fullName) {
+      return;
+    }
+    dispatch(setName(fullName));
+    onNext();
+  }
   return (
     <>
-      <div>Name component</div>
-      <button onClick={onNext}>Next</button>
+      <Card title="What's your full name" icon="goggle-emoji">
+        <TextInput value={fullName} onChange={(e) => setfullName(e.target.value)} />
+        <p className={styles.paragraph}>People use realk names at codershouse :)</p>
+        <div className={styles.actionButtonWrap}>
+          <Button onClick={nextStep} text="Next" />
+        </div>
+      </Card>
     </>
   );
 };
