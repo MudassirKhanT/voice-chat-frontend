5853fbab9d3454f7a467999c6d0d2208e204a8c5
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-09 19:22:27 +0530

    add otp page

--- src/pages/Steps/StepOtp/StepOtp.jsx
+++ src/pages/Steps/StepOtp/StepOtp.jsx
@@ -3,11 +3,23 @@ import Card from "../../../Components/shared/Card/Card";
 import styles from "./StepOtp.module.css";
 import TextInput from "../../../Components/shared/TextInput/TextInput";
 import Button from "../../../Components/shared/Button/Button";
-
+import { verifyOtp } from "../../../http";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setAuth } from "../../../store/authSlice";
 const StepOtp = ({ onNext }) => {
   const [otp, StepOtp] = useState("");
-
-  async function submit() {}
+  const dispatch = useDispatch();
+  const { phone, hash } = useSelector((state) => state.auth.otp);
+  async function submit() {
+    try {
+      const { data } = await verifyOtp({ otp, phone, hash });
+      console.log(data);
+      dispatch(setAuth(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <div className={styles.cardWrapper}>
