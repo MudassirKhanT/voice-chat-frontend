6781a13082e36692ae18e903a21c5abdb7172c0c
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-07-09 19:22:37 +0530

    added phone page

--- src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -3,15 +3,26 @@ import Card from "../../../../Components/shared/Card/Card";
 import Button from "../../../../Components/shared/Button/Button";
 import TextInput from "../../../../Components/shared/TextInput/TextInput";
 import styles from "../StepPhoneEmail.module.css";
+import { sendOtp } from "../../../../http/index";
+import { useDispatch } from "react-redux";
+import { setOtp } from "../../../../store/authSlice";
 const Phone = ({ onNext }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const dispatch = useDispatch();
+  async function submit() {
+    //server request
+    const { data } = await sendOtp({ phone: phoneNumber });
+    console.log(data);
+    dispatch(setOtp({ phone: data.phone, hash: data.hash }));
+    onNext();
+  }
   return (
     <Card title="Enter your phone number" icon="phone">
       <TextInput value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
 
       <div>
         <div className={styles.actionButtonWrap}>
-          <Button text="Next" onClick={onNext}></Button>
+          <Button text="Next" onClick={submit}></Button>
         </div>
         <p className={styles.bottomParagraph}>By entering your number, you’re agreeing to our Terms of Service and Privacy Policy. Thanks!</p>
       </div>
